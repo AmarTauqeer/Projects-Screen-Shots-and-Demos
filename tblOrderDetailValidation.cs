76a27a36a90d2fb500e72804448f278769fd8265
Amar Tauqeer <31622472+AmarTauqeer@users.noreply.github.com>
2019-07-28 14:44:37 +0200

    Add files via upload

---
+++ tblOrderDetailValidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Gentry
+{
+    public class tblOrderDetailValidation
+    {
+        public int OrderId { get; set; }
+        [Required(ErrorMessage = "Product is required.")]
+        public int ProductId { get; set; }
+        [Required(ErrorMessage = "Quantity is required.")]
+        public int Quantity { get; set; }
+        [Required(ErrorMessage = "Rate is required.")]
+        public int Rate { get; set; }
+    }
+    [MetadataType(typeof(tblOrderDetailValidation))]
+    public partial class OrderDetail
+    {
+    }
+}
---
+++ tblSaleOrderValidation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Gentry
+{
+    public class tblSaleOrderValidation
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        [Required(ErrorMessage = "Order date is required.")]
+        [Display(Name = "Order Date")]
+        public System.DateTime OrderDate { get; set; }
+        [Display(Name = "Order Status")]
+        public string OrderStatus { get; set; }
+        [Display(Name = "Customer Name")]
+        public string CustomerName { get; set; }
+    }
+    [MetadataType(typeof(tblSaleOrderValidation))]
+    public partial class OrderMaster
+    {
+    }
+
+
+}
